9d122f3dce9768b563822d8c1a0765d286eef653
IceKing-1912 <71691466+iceking-1912@users.noreply.github.com>
2024-02-17 16:29:03 +0000

    Class,inheritance

--- Basics/Basic.py
+++ Basics/Basic.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # tuple
 # coordinator = [5, 7]
 # coordinator[0] = 13
@@ -99,3 +100,52 @@
 
 
 
+=======
+
+class pen(__name__):
+    def __init__(self,name):
+        self.__name__ = name
+
+    def doesitwrite(self,ink):
+        print(self.__name__)
+        if (ink > 0) and (ink >5):
+            print("Ink- "+str(ink)+"ml")
+            print("Writing")
+        else :
+            # ink += "ml"
+            print("Ink- "+str(ink)+"ml")
+            print("Not Writing")
+
+fountain = pen("fountain")
+
+refil = 4
+
+fountain.doesitwrite("fountain",refil)
+
+
+# ball = pen()
+
+# ball.doesitwrite(refil)
+
+
+
+# class animal:
+    # def walk(self):
+    #     print("walking")
+        
+# class dog(animal):
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+        
+#     def bark(self):
+#         print("woof!!")
+
+# husky = dog("husky",10)
+
+# print(husky.name)
+# print(husky.age)
+# husky.bark()
+# husky.walk()
+
+>>>>>>> 8587056 (Class,inheritance)
