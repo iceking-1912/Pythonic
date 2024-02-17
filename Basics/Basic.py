86f9bc83fa7d0d130c772d438769ad5d7cfadac1
IceKing-1912 <71691466+iceking-1912@users.noreply.github.com>
2024-02-17 22:11:35 +0530

    Class,inheritance

--- Basics/Basic.py
+++ Basics/Basic.py
@@ -99,3 +99,49 @@
 # while x <= 10:
 #     print("meow")
 #     x += 1
+
+
+# class pen(__name__):
+#     def __init__(self, name):
+#         self.__name__ = name
+#
+#     def doesitwrite(self, ink):
+#         print(self.__name__)
+#         if (ink > 0) and (ink > 5):
+#             print("Ink- " + str(ink) + "ml")
+#             print("Writing")
+#         else:
+#             # ink += "ml"
+#             print("Ink- " + str(ink) + "ml")
+#             print("Not Writing")
+#
+#
+# fountain = pen("fountain")
+#
+# refil = 4
+#
+# fountain.doesitwrite("fountain", refil)
+
+# ball = pen()
+
+# ball.doesitwrite(refil)
+
+
+# class animal:
+# def walk(self):
+#     print("walking")
+
+# class dog(animal):
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+
+#     def bark(self):
+#         print("woof!!")
+
+# husky = dog("husky",10)
+
+# print(husky.name)
+# print(husky.age)
+# husky.bark()
+# husky.walk()
